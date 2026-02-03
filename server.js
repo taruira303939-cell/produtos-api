812d61b771673925bb1ba381197529b8079232bf
taruira303939-cell <taruira303939@gmail.com>
2026-02-03 20:45:29 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -6,48 +6,45 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+/* CADASTRAR VINHO */
+app.post("/produtos", async (req, res) => {
+  const { produto, quantidade, } = req.body;
+
+  const result = await pool.query(
+    "INSERT INTO proutos (produto, quantidade) VALUES ($1,$2) RETURNING *",
+    [produto, quantidade]
+  );
+
+  res.json(result.rows[0]);
+});
+
 /* LISTAR TODOS */
 app.get("/produtos", async (req, res) => {
   const result = await pool.query("SELECT * FROM produtos ORDER BY produto");
   res.json(result.rows);
 });
-const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
+
+/*editar*/
+app.put("/produtos/:id", async (req, res) => {
+  const { produto, quantidade } = req.body;
+
+  await pool.query(
+    `
+    UPDATE produtos SET
+      produto = COALESCE($1, produto),
+      quantidade = COALESCE($2, quantidade)
+    WHERE id = $3
+    `,
+    [produto, quantidade, req.params.id]
+  );
+
+  res.sendStatus(200);
 });
 
 
-/* =========================
-   CREATE - inserir produto
-   ========================= */
-app.post("/produtos", async (req, res) => {
-  try {
-    // 1️⃣ Dados que vêm do front-end (JSON)
-    const { produto, quantidade } = req.body;
-
-    // 2️⃣ Validação simples
-    if (!produto || quantidade === undefined) {
-      return res.status(400).json({
-        erro: "Produto e quantidade são obrigatórios"
-      });
-    }
-
-    // 3️⃣ Comando SQL (INSERT)
-    const sql = `
-      INSERT INTO produtos (produto, quantidade)
-      VALUES ($1, $2)
-      RETURNING *
-    `;
-
-    // 4️⃣ Executa o SQL no PostgreSQL
-    const result = await pool.query(sql, [produto, quantidade]);
-
-    // 5️⃣ Retorna o produto salvo
-    res.status(201).json(result.rows[0]);
-
-  } catch (erro) {
-    console.error(erro);
-    res.status(500).json({ erro: "Erro ao inserir produto" });
-  }
+/*excluir*/
+app.delete("/produtos/:id", async (req, res) => {
+  await pool.query("DELETE FROM produtos WHERE id=$1", [req.params.id]);
+  res.sendStatus(200);
 });
