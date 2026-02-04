28636b67e107e69413ddee15584fa2776c91eebf
taruira303939-cell <taruira303939@gmail.com>
2026-02-03 22:22:21 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -48,3 +48,8 @@ app.delete("/produtos/:id", async (req, res) => {
   await pool.query("DELETE FROM produtos WHERE id=$1", [req.params.id]);
   res.sendStatus(200);
 });
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
